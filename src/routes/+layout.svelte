3ec909f4830c0585d8c16518598c838230947773
TheColorman <thoren.alex@gmail.com>
2023-06-15 21:14:42 +0200

    💄 Made styles global

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <main class="static flex min-h-screen flex-col items-center bg-indigo-300">
-	<header class="sticky h-36 w-full overflow-hidden top-0" />
-	<section class="relative mt-2 w-[80vw] rounded-md bg-white p-6 overflow-hidden shadow-xl">
+	<header class="sticky top-0 h-36 w-full overflow-hidden" />
+	<section class="relative mt-2 w-[80vw] overflow-hidden rounded-md bg-white p-6 shadow-xl">
 		<slot />
 	</section>
 </main>
 
-<style>
+<style global>
 	header {
 		background-image: url('/images/header.webp');
 		background-position: center center;
@@ -18,5 +18,8 @@
 	header::before {
 		content: '';
 		@apply absolute inset-0 bg-gradient-to-b from-transparent to-indigo-300;
+	}
+	a {
+		@apply text-sky-500 hover:cursor-pointer hover:text-sky-600 hover:underline;
 	}
 </style>
