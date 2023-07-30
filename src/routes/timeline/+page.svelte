3ac439d68bc8d2a297c4bb1f85349f56a5012c2e
TheColorman <thoren.alex@gmail.com>
2023-07-30 15:35:19 +0200

    ✨ Add initial timeline page

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -1,1 +1,37 @@
-Hello world
+<script lang="ts">
+	import type { VertexMeta, EdgeMeta } from '$lib/types';
+	import { Node, Svelvet, Minimap, Controls, Edge, Anchor } from 'svelvet';
+	import verticesSrc from '$lib/nasuverse/timeline/vertices.json';
+	import edgesSrc from '$lib/nasuverse/timeline/edges.json';
+	import DiGraph from '$lib/Graph';
+	import CustomEdge from '$lib/components/CustomEdge.svelte';
+
+	const vertexSize = { width: 200, height: 100 };
+
+	const vertices = verticesSrc as VertexMeta[];
+	const edges = edgesSrc as EdgeMeta[];
+
+	const graph = new DiGraph(vertices, {
+		vertexSize,
+		gap: 50
+	});
+
+	for (const edge of edges) {
+		graph.addEdge(edge.from, edge.to);
+	}
+
+	const vertexMap = graph.Vertices;
+</script>
+
+<Svelvet minimap>
+	{#each [...vertexMap.values()] as vertex}
+		<Node
+			id={vertex.id}
+			position={{ x: vertex.position.x, y: vertex.position.y }}
+			dimensions={vertexSize}
+			label={vertex.metadata.name}
+			connections={graph.adjacent(vertex.id)}
+			edge={CustomEdge}
+		/>
+	{/each}
+</Svelvet>
