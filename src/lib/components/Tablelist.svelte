774dab8af6c86050f391ef13bb543acf2324e157
TheColorman <thoren.alex@gmail.com>
2024-03-18 18:32:54 +0100

    Add: icons

--- src/lib/components/Tablelist.svelte
+++ src/lib/components/Tablelist.svelte
@@ -321,6 +321,70 @@
 				return 'Download';
 		}
 	}
+
+	function displayNote(note: string) {
+		if (!note) return;
+
+		switch (note) {
+			case 'format':
+				return `<span class="inline-block float-right" style="color: #3b82f6" title="Format should be updated">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 -960 960 960"
+							stroke="currentColor"
+							fill="currentColor"
+							class="h-4 w-4"
+							style="transform: translate(0, 0.2rem)"
+							><path
+								d="M240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h480q33 0 56.5 23.5T800-800v640q0 33-23.5 56.5T720-80H240Zm0-80h480v-640h-80v280l-100-60-100 60v-280H240v640Zm0 0v-640 640Zm200-360 100-60 100 60-100-60-100 60Z"
+							/></svg
+						>
+					</span>`;
+			case 'translation-none':
+				return `<span class="inline-block float-right" style="color: #f63939" title="Not translated">
+					<svg
+						xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 -960 960 960" 
+						stroke="currentColor"
+						fill="currentColor"
+						class=h-4 w-4"
+						style="transform: translate(0, 0.2rem)"
+						><path
+							d="m476-80 182-480h84L924-80h-84l-43-122H603L560-80h-84ZM160-200l-56-56 202-202q-35-35-63.5-80T190-640h84q20 39 40 68t48 58q33-33 68.5-92.5T484-720H40v-80h280v-80h80v80h280v80H564q-21 72-63 148t-83 116l96 98-30 82-122-125-202 201Zm468-72h144l-72-204-72 204Z"
+						/></svg
+					>
+				</span>`;
+			case 'translation-partly':
+				return `<span class="inline-block float-right" style="color: #f6af3a" title="Only partly translated">
+					<svg
+						xmlns="http://www.w3.org/2000/svg" 
+						viewBox="0 -960 960 960" 
+						stroke="currentColor"
+						fill="currentColor"
+						class=h-4 w-4"
+						style="transform: translate(0, 0.2rem)"
+						><path
+							d="m476-80 182-480h84L924-80h-84l-43-122H603L560-80h-84ZM160-200l-56-56 202-202q-35-35-63.5-80T190-640h84q20 39 40 68t48 58q33-33 68.5-92.5T484-720H40v-80h280v-80h80v80h280v80H564q-21 72-63 148t-83 116l96 98-30 82-122-125-202 201Zm468-72h144l-72-204-72 204Z"
+						/></svg
+					>
+				</span>`;
+			case 'missing-material':
+				return `<span class="inline-block float-right" style="color: #f63939" title="Missing material. Contact me if you have a source!">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 -960 960 960"
+						stroke="currentColor"
+						fill="currentColor"
+						class=h-4 w-4"
+						style="transform: translate(0, 0.2rem)"
+						><path
+							d="M240-80q-33 0-56.5-23.5T160-160v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-80H240Zm-80-440v-280q0-33 23.5-56.5T240-880h320l240 240v120h-80v-80H520v-200H240v280h-80ZM40-360v-80h880v80H40Zm440-160Zm0 240Z"
+						/></svg>
+					</span>`;
+			default:
+				return '';
+		}
+	}
 </script>
 
 <!-- Set head if url contains permalink to entry -->
@@ -400,7 +464,8 @@
 					<a
 						bind:this={accordionLinks[entry.id]}
 						on:click|stopPropagation={(event) => expandAll(event, entry.id)}
-						on:mouseover={() => metadata[entry.id].cover && preload(`/images/items/${metadata[entry.id].cover}`)}
+						on:mouseover={() =>
+							metadata[entry.id].cover && preload(`/images/items/${metadata[entry.id].cover}`)}
 						on:focus={() => preload(`/images/items/${metadata[entry.id].cover}`)}>{entry.title}</a
 					>
 					<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
@@ -428,6 +493,11 @@
 							/>
 						</svg>
 					</a>
+					{#if entry.notes !== undefined}
+						{#each entry.notes as note}
+							{@html displayNote(note)}
+						{/each}
+					{/if}
 				{:else}
 					<p class="inline-block">{entry.title}</p>
 					<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
