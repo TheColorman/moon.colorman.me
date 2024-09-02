4a9b03a3c08a35ae225c88fbe09a64a41e8520d9
TheColorman <github@colorman.me>
2024-09-02 21:54:10 +1000

    Add: ability to toggle unofficial works

--- src/lib/components/Tablelist.svelte
+++ src/lib/components/Tablelist.svelte
@@ -14,6 +14,10 @@
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import Switch from '$lib/components/Switch.svelte';
+
+	// Track whether to show unofficial entries
+	let showUnofficial = 'on';
 
 	// Copy entries to a new array so we can sort it
 	let entries = [...entriesImmutable];
@@ -370,253 +374,268 @@
 	{/if}
 </svelte:head>
 
-<table class="ml-16 w-[64rem] max-w-5xl md:ml-0">
-	<tr class="text-left hover:bg-gray-100 dark:hover:bg-[#293548]">
-		{#each columnLabels as { key, label, width }}
-			<th on:click={() => sortEntries(key)} style={`width: ${width}rem`}>
-				{label}
-				<div class="relative -ml-0.5 mb-2 mr-2 inline-block">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="absolute -top-1 h-3 w-3 {sortList[key] === 'desc' ? 'block' : 'hidden'}"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
-					</svg>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="absolute -top-1 h-3 w-3 {sortList[key] === 'asc' ? 'block' : 'hidden'}"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-					</svg>
-				</div>
-			</th>
-		{/each}
-	</tr>
-	{#each entries as entry, i}
-		<tr
-			class="group border-t dark:border-[#2e3c52] {$completed[entry.id]
-				? 'bg-gray-200 text-gray-500 hover:bg-gray-300 dark:bg-[#293548] dark:text-gray-400 dark:hover:bg-[#2e3c52]'
-				: 'hover:bg-gray-100 dark:hover:bg-[#293548]'}"
-			id={`${entry.id}`}
-		>
-			<td class="py-1">
-				<input
-					type="checkbox"
-					class="hover:cursor-pointer"
-					bind:checked={$completed[entry.id]}
-					on:click={(event) => toggleRow(event, entry.id)}
-				/>
-			</td>
-			<td class="relative">
-				{#if metadata[entry.id]}
-					<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
-					<a
-						bind:this={accordionLinks[entry.id]}
-						on:click|stopPropagation={(event) => expandAll(event, entry.id)}
-						on:mouseover={() =>
-							metadata[entry.id].cover && preload(`/images/items/${metadata[entry.id].cover}`)}
-						on:focus={() => preload(`/images/items/${metadata[entry.id].cover}`)}>{entry.title}</a
-					>
-					<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
-					<a
-						class="hidden hover:cursor-pointer hover:!text-gray-700 group-hover:inline-block group-hover:text-gray-400 dark:hover:!text-gray-300 dark:group-hover:text-gray-600"
-						title="Copy permalink"
-						href="/{entry.id}#{entry.id}"
-						on:click|stopPropagation|preventDefault={(event) => {
-							goto(`${entry.id}/#${entry.id}`);
-							navigator.clipboard.writeText(`https://nasu.colorman.me/${entry.id}#${entry.id}`);
-						}}
-					>
+<div class="mt-8">
+	<Switch
+		bind:value={showUnofficial}
+		label="Show unofficial entries"
+		fontSize={16}
+		design="slider"
+	/>
+	<table class="ml-16 w-[64rem] max-w-5xl md:ml-0">
+		<tr class="text-left hover:bg-gray-100 dark:hover:bg-[#293548]">
+			{#each columnLabels as { key, label, width }}
+				<th on:click={() => sortEntries(key)} style={`width: ${width}rem`}>
+					{label}
+					<div class="relative -ml-0.5 mb-2 mr-2 inline-block">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							class="h-4 w-4 translate-y-0.5"
+							class="absolute -top-1 h-3 w-3 {sortList[key] === 'desc' ? 'block' : 'hidden'}"
 						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-							/>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
 						</svg>
-					</a>
-					{#if entry.notes !== undefined}
-						{#each entry.notes as note}
-							<span class="float-right">
-								{@html displayNote(note)}
-							</span>
-						{/each}
-					{/if}
-				{:else}
-					<p class="inline-block">{entry.title}</p>
-					<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
-					<p
-						class="hidden hover:cursor-pointer hover:!text-gray-700 group-hover:inline-block group-hover:text-gray-400 dark:hover:!text-gray-300 dark:group-hover:text-gray-600"
-						title="Copy permalink"
-						on:click|stopPropagation|preventDefault={(event) => {
-							goto(`/#${entry.id}`);
-							navigator.clipboard.writeText(`https://nasu.colorman.me/#${entry.id}`);
-						}}
-					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							class="h-4 w-4 translate-y-0.5"
+							class="absolute -top-1 h-3 w-3 {sortList[key] === 'asc' ? 'block' : 'hidden'}"
 						>
 							<path
 								stroke-linecap="round"
 								stroke-linejoin="round"
-								d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+								d="M19.5 8.25l-7.5 7.5-7.5-7.5"
 							/>
 						</svg>
-					</p>
-				{/if}
-			</td>
-			<td>{entry.released}</td>
-			<td>{entry.ended}</td>
-			<td>{entry.medium}</td>
-		</tr>
-		{#if metadata[entry.id]}
-			<AccordionRow
-				colspan={columnLabels.length}
-				trigger={accordionLinks[entry.id]}
-				class={`border-t bg-gray-100 dark:border-[#2e3c52] dark:bg-[#293548] dark:text-gray-200`}
-				bind:this={accordionRefs[entry.id]}
-			>
-				<div class="m-2 mx-3 flex max-w-full">
-					{#if metadata[entry.id].cover}
-						<img
-							src={`/images/items/${metadata[entry.id].cover}`}
-							alt={`${entry.title} cover`}
-							class="mr-4 inline h-full w-44 rounded-md"
-						/>
-					{/if}
-					<div class="relative grid w-full grid-cols-3 gap-4">
-						{#if metadata[entry.id].external}
-							<div class="absolute right-1 top-1 flex gap-2">
-								{#if metadata[entry.id].external.anilist}
-									<a
-										href={metadata[entry.id].external.anilist}
-										target="_blank"
-										rel="noopener noreferrer"
-										title="Go to AniList entry"
-									>
-										<img src="/images/anilist.ico" alt="AniList" class="w-4" />
-									</a>
-								{/if}
-								{#if metadata[entry.id].external.myanimelist}
-									<a
-										href={metadata[entry.id].external.myanimelist}
-										target="_blank"
-										rel="noopener noreferrer"
-										title="Go to MyAnimeList entry"
-									>
-										<img src="/images/myanimelist.ico" alt="MyAnimeList" class="w-4" />
-									</a>
-								{/if}
-								{#if metadata[entry.id].external.vndb}
-									<a
-										href={metadata[entry.id].external.vndb}
-										target="_blank"
-										rel="noopener noreferrer"
-										title="Go to the visual novel database entry"
-									>
-										<img src="/images/vndb.ico" alt="vndb" class="w-4" />
-									</a>
-								{/if}
-								{#if metadata[entry.id].external.wiki}
-									<a
-										href={metadata[entry.id].external.wiki}
-										target="_blank"
-										rel="noopener noreferrer"
-										title="Go to Type-Moon wiki entry"
-									>
-										<img src="/images/wiki.ico" alt="Wiki" class="w-4" />
-									</a>
-								{/if}
-							</div>
-						{/if}
-						<div class="col-span-2 w-full">
-							<p class="whitespace-pre-line">
-								{@html metadata[entry.id].description || 'No description available.'}
-							</p>
-							{#if Object.keys(metadata[entry.id].source).length}
-								<h1 class="mt-4 text-xl font-thin">Download source</h1>
-								<ul>
-									{#each Object.entries(metadata[entry.id].source) as [key, value]}
-										<li>
-											<a href={value} target="_blank" rel="noopener noreferrer">{key}</a>
-										</li>
-									{/each}
-								</ul>
-							{/if}
-							{#if metadata[entry.id].credit}
-								<h2 class="mt-2 text-lg font-thin">Download credit:</h2>
-								<p class="whitespace-pre-line">
-									{@html metadata[entry.id].credit}
-								</p>
-							{/if}
-						</div>
-
-						<div class="w-full">
-							<h1 class="mt-6 text-xl font-thin md:mt-0">Downloads</h1>
-							<ul>
-								{#if Object.keys(metadata[entry.id].download).length}
-									{#each Object.entries(metadata[entry.id].download) as [key, value]}
-										<li>
-											{#if value.startsWith('#')}
-												<a
-													href={value}
-													on:click={(event) => {
-														const target = accordionRefs[value.slice(1)];
-														console.log(target?.getOpenState());
-														if (!target?.getOpenState()) {
-															target?.toggle();
-														}
-													}}>{key}</a
-												>
-											{:else}
-												<a href={value} target="_blank" rel="noopener noreferrer">{key}</a>
-											{/if}
-										</li>
-									{/each}
-								{:else}
-									<li>Nothing here!</li>
-								{/if}
-							</ul>
-
-							<h1 class="mt-4 text-xl font-thin">Official links</h1>
-							<ul>
-								{#if Object.keys(metadata[entry.id].official).length}
-									{#each Object.entries(metadata[entry.id].official) as [key, value]}
-										<li>
-											<a href={value} target="_blank" rel="noopener noreferrer">{key}</a>
-										</li>
-									{/each}
-								{:else}
-									<li>Nothing here!</li>
-								{/if}
-							</ul>
-						</div>
 					</div>
-				</div>
-			</AccordionRow>
-		{/if}
-	{/each}
-</table>
+				</th>
+			{/each}
+		</tr>
+		{#each entries as entry, i}
+			{#if !entry.notes?.includes('unofficial') || showUnofficial == 'on'}
+				<tr
+					class="group border-t dark:border-[#2e3c52] {$completed[entry.id]
+						? 'bg-gray-200 text-gray-500 hover:bg-gray-300 dark:bg-[#293548] dark:text-gray-400 dark:hover:bg-[#2e3c52]'
+						: 'hover:bg-gray-100 dark:hover:bg-[#293548]'}"
+					id={`${entry.id}`}
+				>
+					<td class="py-1">
+						<input
+							type="checkbox"
+							class="hover:cursor-pointer"
+							bind:checked={$completed[entry.id]}
+							on:click={(event) => toggleRow(event, entry.id)}
+						/>
+					</td>
+					<td class="relative">
+						{#if metadata[entry.id]}
+							<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
+							<a
+								bind:this={accordionLinks[entry.id]}
+								on:click|stopPropagation={(event) => expandAll(event, entry.id)}
+								on:mouseover={() =>
+									metadata[entry.id].cover && preload(`/images/items/${metadata[entry.id].cover}`)}
+								on:focus={() => preload(`/images/items/${metadata[entry.id].cover}`)}
+								>{entry.title}</a
+							>
+							<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
+							<a
+								class="hidden hover:cursor-pointer hover:!text-gray-700 group-hover:inline-block group-hover:text-gray-400 dark:hover:!text-gray-300 dark:group-hover:text-gray-600"
+								title="Copy permalink"
+								href="/{entry.id}#{entry.id}"
+								on:click|stopPropagation|preventDefault={(event) => {
+									goto(`${entry.id}/#${entry.id}`);
+									navigator.clipboard.writeText(`https://nasu.colorman.me/${entry.id}#${entry.id}`);
+								}}
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="h-4 w-4 translate-y-0.5"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+									/>
+								</svg>
+							</a>
+							{#if entry.notes !== undefined}
+								{#each entry.notes as note}
+									<span class="float-right">
+										{@html displayNote(note)}
+									</span>
+								{/each}
+							{/if}
+						{:else}
+							<p class="inline-block">{entry.title}</p>
+							<!-- svelte-ignore a11y-missing-attribute a11y-click-events-have-key-events -->
+							<p
+								class="hidden hover:cursor-pointer hover:!text-gray-700 group-hover:inline-block group-hover:text-gray-400 dark:hover:!text-gray-300 dark:group-hover:text-gray-600"
+								title="Copy permalink"
+								on:click|stopPropagation|preventDefault={(event) => {
+									goto(`/#${entry.id}`);
+									navigator.clipboard.writeText(`https://nasu.colorman.me/#${entry.id}`);
+								}}
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="h-4 w-4 translate-y-0.5"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+									/>
+								</svg>
+							</p>
+						{/if}
+					</td>
+					<td>{entry.released}</td>
+					<td>{entry.ended}</td>
+					<td>{entry.medium}</td>
+				</tr>
+				{#if metadata[entry.id]}
+					<AccordionRow
+						colspan={columnLabels.length}
+						trigger={accordionLinks[entry.id]}
+						class={`border-t bg-gray-100 dark:border-[#2e3c52] dark:bg-[#293548] dark:text-gray-200`}
+						bind:this={accordionRefs[entry.id]}
+					>
+						<div class="m-2 mx-3 flex max-w-full">
+							{#if metadata[entry.id].cover}
+								<img
+									src={`/images/items/${metadata[entry.id].cover}`}
+									alt={`${entry.title} cover`}
+									class="mr-4 inline h-full w-44 rounded-md"
+								/>
+							{/if}
+							<div class="relative grid w-full grid-cols-3 gap-4">
+								{#if metadata[entry.id].external}
+									<div class="absolute right-1 top-1 flex gap-2">
+										{#if metadata[entry.id].external.anilist}
+											<a
+												href={metadata[entry.id].external.anilist}
+												target="_blank"
+												rel="noopener noreferrer"
+												title="Go to AniList entry"
+											>
+												<img src="/images/anilist.ico" alt="AniList" class="w-4" />
+											</a>
+										{/if}
+										{#if metadata[entry.id].external.myanimelist}
+											<a
+												href={metadata[entry.id].external.myanimelist}
+												target="_blank"
+												rel="noopener noreferrer"
+												title="Go to MyAnimeList entry"
+											>
+												<img src="/images/myanimelist.ico" alt="MyAnimeList" class="w-4" />
+											</a>
+										{/if}
+										{#if metadata[entry.id].external.vndb}
+											<a
+												href={metadata[entry.id].external.vndb}
+												target="_blank"
+												rel="noopener noreferrer"
+												title="Go to the visual novel database entry"
+											>
+												<img src="/images/vndb.ico" alt="vndb" class="w-4" />
+											</a>
+										{/if}
+										{#if metadata[entry.id].external.wiki}
+											<a
+												href={metadata[entry.id].external.wiki}
+												target="_blank"
+												rel="noopener noreferrer"
+												title="Go to Type-Moon wiki entry"
+											>
+												<img src="/images/wiki.ico" alt="Wiki" class="w-4" />
+											</a>
+										{/if}
+									</div>
+								{/if}
+								<div class="col-span-2 w-full">
+									<p class="whitespace-pre-line">
+										{@html metadata[entry.id].description || 'No description available.'}
+									</p>
+									{#if Object.keys(metadata[entry.id].source).length}
+										<h1 class="mt-4 text-xl font-thin">Download source</h1>
+										<ul>
+											{#each Object.entries(metadata[entry.id].source) as [key, value]}
+												<li>
+													<a href={value} target="_blank" rel="noopener noreferrer">{key}</a>
+												</li>
+											{/each}
+										</ul>
+									{/if}
+									{#if metadata[entry.id].credit}
+										<h2 class="mt-2 text-lg font-thin">Download credit:</h2>
+										<p class="whitespace-pre-line">
+											{@html metadata[entry.id].credit}
+										</p>
+									{/if}
+								</div>
+
+								<div class="w-full">
+									<h1 class="mt-6 text-xl font-thin md:mt-0">Downloads</h1>
+									<ul>
+										{#if Object.keys(metadata[entry.id].download).length}
+											{#each Object.entries(metadata[entry.id].download) as [key, value]}
+												<li>
+													{#if value.startsWith('#')}
+														<a
+															href={value}
+															on:click={(event) => {
+																const target = accordionRefs[value.slice(1)];
+																console.log(target?.getOpenState());
+																if (!target?.getOpenState()) {
+																	target?.toggle();
+																}
+															}}>{key}</a
+														>
+													{:else}
+														<a href={value} target="_blank" rel="noopener noreferrer">{key}</a>
+													{/if}
+												</li>
+											{/each}
+										{:else}
+											<li>Nothing here!</li>
+										{/if}
+									</ul>
+
+									<h1 class="mt-4 text-xl font-thin">Official links</h1>
+									<ul>
+										{#if Object.keys(metadata[entry.id].official).length}
+											{#each Object.entries(metadata[entry.id].official) as [key, value]}
+												<li>
+													<a href={value} target="_blank" rel="noopener noreferrer">{key}</a>
+												</li>
+											{/each}
+										{:else}
+											<li>Nothing here!</li>
+										{/if}
+									</ul>
+								</div>
+							</div>
+						</div>
+					</AccordionRow>
+				{/if}
+			{/if}
+		{/each}
+	</table>
+</div>
 
 <style>
 	th {
