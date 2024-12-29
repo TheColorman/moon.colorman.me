c5acff4bac8835adf66afd1db9eaa27c0c7a2739
TheColorman <github@colorman.me>
2024-12-29 20:59:24 +1100

    Update devshell and add direnv

--- flake.nix
+++ flake.nix
@@ -6,16 +6,12 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [pnpm nodejs_22];
-      shellHook = ''
-        code .
-      '';
     };
   };
 }
