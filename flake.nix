d458613b19df540b65edf62ff1354ef626c05da2
Colorman <foss@colorman.me>
2026-08-11 18:40:20 +0200

    nix: add prettierd

--- flake.nix
+++ flake.nix
@@ -3,11 +3,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = with pkgs; [pnpm nodejs];
+  outputs =
+    { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          pnpm
+          nodejs
+          prettierd
+        ];
+      };
     };
-  };
 }
